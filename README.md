f3da3e27fb80310a478ceb6945f0007121f48b66
developer <developer@quantitymeasurement.com>
2026-03-19 10:30:00 +0530

    Coded UC3 for QuantityMeasurement to fetch units by type

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Use Cases Accomplished:
 - UC1: create JSON Server Database
 - UC2: initialise app on page load
+- UC3: fetch units by type
--- js/api.js
+++ js/api.js
@@ -4,3 +4,9 @@
  * @version - 1.0
  */
 const API_BASE = 'http://localhost:3000';
+
+async function getUnits(type) {
+    const res = await fetch(`${API_BASE}/units?type=${type.toLowerCase()}`);
+    if (!res.ok) throw new Error('Network error');
+    return res.json();
+}
